802810bbcb295b49c55561d6c9d2f45e40becf26
rrapolu12 <50491673+rrapolu12@users.noreply.github.com>
2023-12-01 17:54:10 -0500

    Update extract_images_into_arrays.sh

--- extract_images_into_arrays.sh
+++ extract_images_into_arrays.sh
@@ -1,57 +1,73 @@
 #!/bin/bash
 
-# Define arrays to store lines under different sections
-declare -a images
-declare -a helmcharts
-declare -a rpms
+parse_yaml() {
+    local yaml_file="$1"
 
-# Set the flag to false initially
-in_images=false
-in_helmcharts=false
-in_rpms=false
+    # Define arrays to store lines under different sections
+    declare -a images
+    declare -a helmcharts
+    declare -a rpms
 
-# Read the YAML file line by line
-while IFS= read -r line; do
-    # Check if the line contains the start of the 'images' section
-    if [[ "$line" =~ ^images: ]]; then
-        in_images=true
-        in_helmcharts=false
-        in_rpms=false
-    # Check if the line contains the start of the 'helmcharts' section
-    elif [[ "$line" =~ ^helmcharts: ]]; then
-        in_images=false
-        in_helmcharts=true
-        in_rpms=false
-    # Check if the line contains the start of the 'rpms' section
-    elif [[ "$line" =~ ^rpms: ]]; then
-        in_images=false
-        in_helmcharts=false
-        in_rpms=true
-    # Check if the line is not empty and is not a comment
-    elif [[ -n "$line" && ! "$line" =~ ^\s*# ]]; then
-        # Depending on the current section, append the line to the corresponding array
-        if [ "$in_images" = true ]; then
-            images+=("$line")
-        elif [ "$in_helmcharts" = true ]; then
-            helmcharts+=("$line")
-        elif [ "$in_rpms" = true ]; then
-            rpms+=("$line")
+    # Set the flag to false initially
+    in_images=false
+    in_helmcharts=false
+    in_rpms=false
+
+    # Read the YAML file line by line
+    while IFS= read -r line; do
+        # Check if the line contains the start of the 'images' section
+        if [[ "$line" =~ ^images: ]]; then
+            in_images=true
+            in_helmcharts=false
+            in_rpms=false
+        # Check if the line contains the start of the 'helmcharts' section
+        elif [[ "$line" =~ ^helmcharts: ]]; then
+            in_images=false
+            in_helmcharts=true
+            in_rpms=false
+        # Check if the line contains the start of the 'rpms' section
+        elif [[ "$line" =~ ^rpms: ]]; then
+            in_images=false
+            in_helmcharts=false
+            in_rpms=true
+        # Check if the line is not empty and is not a comment
+        elif [[ -n "$line" && ! "$line" =~ ^\s*# ]]; then
+            # Depending on the current section, append the line to the corresponding array
+            if [ "$in_images" = true ]; then
+                images+=("$line")
+            elif [ "$in_helmcharts" = true ]; then
+                helmcharts+=("$line")
+            elif [ "$in_rpms" = true ]; then
+                rpms+=("$line")
+            fi
         fi
-    fi
-done < repo.yaml
+    done < "$yaml_file"
 
-# Print the contents of the arrays (optional)
-echo "Images:"
-for image in "${images[@]}"; do
+    # Print the contents of the arrays (optional)
+    echo "Images:"
+    for image in "${images[@]}"; do
+        echo "  $image"
+    done
+
+    echo "Helmcharts:"
+    for helmchart in "${helmcharts[@]}"; do
+        echo "  $helmchart"
+    done
+
+    echo "RPMs:"
+    for rpm in "${rpms[@]}"; do
+        echo "  $rpm"
+    done
+
+    # Return the images array
+    echo "${images[@]}"
+}
+
+# Call the function with the YAML file path
+images_array=($(parse_yaml "your_yaml_file.yaml"))
+
+# Print the images array outside the function
+echo "Images outside the function:"
+for image in "${images_array[@]}"; do
     echo "  $image"
-done
-
-echo "Helmcharts:"
-for helmchart in "${helmcharts[@]}"; do
-    echo "  $helmchart"
-done
-
-echo "RPMs:"
-for rpm in "${rpms[@]}"; do
-    echo "  $rpm"
 done
